69eea80be6ab7fe805f8404593ebab885537d7b4
Steven Espinoza <espisteven5@gmail.com>
2023-10-16 20:00:54 -0700

    Added Javascript

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+const foot = document.createElement('p');
+foot.className = 'text-center text-body-secondary';
+foot.textContent = '© 2023 Steven';
+const footer = document.querySelector('footer');
+footer.appendChild(foot);
+
+//mouseover on the cards to make them bigger https://developer.mozilla.org/en-US/docs/Web/API/Element/mouseover_event
+const cards = document.querySelectorAll('.card');
+cards.forEach(card => {
+    card.addEventListener('mouseover', () => {
+        card.style.transform = 'scale(1.05)';
+        card.style.transition = 'transform 0.3s';
+    });
+    card.addEventListener('mouseout', () => {
+        card.style.transform = 'scale(1)';
+    });
+});
+
